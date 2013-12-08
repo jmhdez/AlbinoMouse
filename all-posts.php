86d16e691b6d433368a9cc270db4b9db69e06778
Juan M. Hernández <juan.hernandez.arroyo@gmail.com>
2013-12-08 13:08:58 +0100

    Added basic text to all-posts page

--- all-posts.php
+++ all-posts.php
@@ -11,37 +11,47 @@ get_header(); ?>
 
 <div id="content" class="site-archive" role="main">
 
-	<div class="row">
-		<div class="span12">Aquí va la presentación de quién soy</div>
+	<div class="intro">
+
+		<h1>Hola, soy Juan María Hernández y éste es mi blog</h1>
+		
+		<p>Me dedico a desarrollar software desde hace unos cuantos años y me encanta aprender cosas nuevas. 
+		Como creo que la mejor forma de aprender algo es tratar de enseñárselo a los demas, escribo este blog
+		donde podrás encontrar información sobre desarrollo de software en general.</p>
+
+		<p>Si quieres saber más sobre mi (enlace a quien soy), ver mis cosas online, etc.</p>
+		
 	</div>
 
-	ESTOY INTENTADO ARREGLAR ESTO PARA QUE SALGAN LAS COLUMNAS COMO QUIERO. FALLA PORQUE ALBINO NO USA BOOTSTRAP. AYS!
+	<div class="three_fourth_col">
 
-	<div class="row">
-		<div class="three_fourth_col">
-			<ul id="archives-posts">
-				<?php
-					global $post;
-					$args = array( 'orderby' => 'post_date', 'order' => 'DESC', 'numberposts' => '10000' );
-					$myposts = get_posts( $args );
+		<h2>Todos mis artículos</h2>
+1
+		<ul id="archives-posts">
+			<?php
+				global $post;
+				$args = array( 'orderby' => 'post_date', 'order' => 'DESC', 'numberposts' => '10000' );
+				$myposts = get_posts( $args );
 
-					foreach( $myposts as $post ) : setup_postdata($post); ?>
-					<li>
-          	<p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br/><small><?php the_date(); ?></small></p>
-        	</li>
-					<?php endforeach; ?>
-			</ul>
-		</div>
-		<div class="one_forth_column">
-			<ul id="archives-tags">
-				<?php 
-					$tags = get_tags();
-					foreach ($tags as $tag) {
-						echo '<li><a href="' . get_tag_link($tag->term_id) . '" title="Posts sobre ' . $tag->name . '">' . $tag->name . '</a></li>' ;
-					}
-				?>
-			</ul>
-		</div>
+				foreach( $myposts as $post ) : setup_postdata($post); ?>
+				<li>
+        	<p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br/><small><?php the_date(); ?></small></p>
+      	</li>
+				<?php endforeach; ?>
+		</ul>
+	</div>
+	<div class="one_forth_column">
+
+		<h2>Temas que he tratado</h2>
+
+		<ul id="archives-tags">
+			<?php 
+				$tags = get_tags();
+				foreach ($tags as $tag) {
+					echo '<li><a href="' . get_tag_link($tag->term_id) . '" title="Posts sobre ' . $tag->name . '">' . $tag->name . '</a></li>' ;
+				}
+			?>
+		</ul>
 	</div>
 
 </div><!-- #content .site-archive -->
