55278f3420bd1b72849c23c5047e93f840ab6075
jmhdez <juan.hernandez.arroyo@gmail.com>
2013-12-11 11:10:41 +0100

    Changed post footer in lists

--- content.php
+++ content.php
@@ -8,20 +8,38 @@
 <?php $options = get_option( 'albinomouse' ); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">	
-		<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'albinomouse' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
-			<?php if ( is_sticky() ) : // Only display if is sticky
-			echo __( '<span class="featured">Featured</span>', 'albinomouse' );
-			endif; ?>
-			<?php if ( 'post' == get_post_type() ) :
-				if ( get_post_format() ) : ?>
-					<span class="post-format-icon format-<?php echo get_post_format(); ?>"></span>
-				<?php else: ?>
-					<span class="post-format-icon format-standard"></span>
-				<?php endif; // End if get_post_format
-			endif; // End if 'post' == get_post_type ?>
-			<h2 class="entry-title"><?php the_title(); ?></h2>
-		</a>
+	<header class="entry-header">
+		<h2 class="entry-title">	
+			<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'albinomouse' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
+				<?php if ( is_sticky() ) : // Only display if is sticky
+				echo __( '<span class="featured">Featured</span>', 'albinomouse' );
+				endif; ?>
+				<?php if ( 'post' == get_post_type() ) :
+					if ( get_post_format() ) : ?>
+						<span class="post-format-icon format-<?php echo get_post_format(); ?>"></span>
+					<?php else: ?>
+						<span class="post-format-icon format-standard"></span>
+					<?php endif; // End if get_post_format
+				endif; // End if 'post' == get_post_type ?>
+				<h2 class="entry-title"><?php the_title(); ?></h2>
+			</a>
+			<br/>
+			<small><i class="icon-calendar"></i> <?php the_time(get_option('date_format')); ?>
+				<?php // translators: used between list items, there is a space after the comma
+				$tags_list = get_the_tag_list( '', __( ', ', 'albinomouse' ) );
+				if ($tags_list) : ?>
+				<span class="sep tags-link"> | </span>
+				<span class="tag-links">
+					<i class="icon-tag"></i>
+					<?php printf($tags_list); ?>
+				</span>
+				<?php endif; // End if $tags_list ?>
+				<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+  				<span class="sep comments-link"> | </span>
+					<span class="comments-link"><i class="icon-comment"> </i><?php comments_popup_link( __( 'Leave a comment', 'albinomouse' ), __( '1 Comment', 'albinomouse' ), __( '% Comments', 'albinomouse' ) ); ?> </span>
+				<?php endif; ?>		
+			</small>
+		</h2>
 	</header><!-- End .entry-header -->
 	<div class="entry-summary clear">
 			<?php if (has_excerpt()) {
@@ -39,40 +57,6 @@
 	</div><!-- End .entry-summary -->
 	<footer class="entry-footer-meta clear">
 	<?php if ( 'post' == get_post_type() ) : ?>
-	
-		<p class="post-info">		
-			<?php
-			$arc_year = get_the_time('Y'); 
-			$arc_month = get_the_time('m'); 
-			$arc_day = get_the_time('d');
-			?>
-			<span class="release-date"><i class="icon-calendar"></i> <a href="<?php echo get_day_link($arc_year, $arc_month, $arc_day); ?>"><?php the_time(get_option('date_format')); ?></a></span>
-			<span class="sep author-link"> | </span>
-			<span class="author-link"><i class="icon-user"></i> <?php the_author_link(); ?> </span>
-			<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-				<span class="sep comments-link"> | </span>
-				<span class="comments-link"><i class="icon-comment"> </i><?php comments_popup_link( __( 'Leave a comment', 'albinomouse' ), __( '1 Comment', 'albinomouse' ), __( '% Comments', 'albinomouse' ) ); ?> </span>
-			<?php endif; ?>		
-			<br/>					
-			<?php // translators: used between list items, there is a space after the comma
-			$categories_list = get_the_category_list( __( ', ', 'albinomouse' ) );
-			if ( $categories_list && albinomouse_categorized_blog() ) : ?>
-			<span class="cat-links">
-				<i class="icon-folder-open"></i>
-				<?php printf($categories_list); ?>
-			</span>
-			<br/>
-			<?php endif; // End if categories ?>
-			<?php // translators: used between list items, there is a space after the comma
-			$tags_list = get_the_tag_list( '', __( ', ', 'albinomouse' ) );
-			if ($tags_list) : ?>
-			<span class="tag-links">
-				<i class="icon-tag"></i>
-				<?php printf($tags_list); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		</p><!--End .post-infos -->
-		
 		<div class="social-buttons clear">
 		<?php if(isset($options['social-media-location']['post']) and $options['social-media-location']['post'] == '1' ) :
 			// Twitter Button
